edade04b4f680e58b10a40b707d0539a118e340d
implaymo <goncalomoreirasilva@gmail.com>
2024-06-25 16:24:34 +0100

    Start Spring Boot project

---
+++ CatGameWesbite/src/main/java/com/catgame/CatGameWesbite/CatGameWesbiteApplication.java
@@ -0,0 +1,13 @@
+package com.catgame.CatGameWesbite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatGameWesbiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatGameWesbiteApplication.class, args);
+	}
+
+}
---
+++ CatGameWesbite/src/test/java/com/catgame/CatGameWesbite/CatGameWesbiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catgame.CatGameWesbite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatGameWesbiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
