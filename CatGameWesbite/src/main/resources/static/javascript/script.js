93ce23fb535b37f8ae63fab35f38409eb2a15259
implaymo <goncalomoreirasilva@gmail.com>
2024-06-28 15:20:25 +0100

    Manage to delete new cat created.

--- CatGameWesbite/src/main/resources/static/javascript/script.js
+++ CatGameWesbite/src/main/resources/static/javascript/script.js
@@ -6,14 +6,15 @@ function catClicked() {
     var catPos = 0;
     clearInterval(id);
     id = setInterval(frame, 10);
+    newCat = createNewCat();
     function frame() {
         if (catPos >= 700) {
             console.log("Cat reach the limits.");
+            removeNewCat(newCat);
             clearInterval(id);
         }
         else {
             console.log("Cat is going to move.");
-            createNewCat();
             catPos++;
             cat.style.left = catPos + 'px';
             cat.style.top = catPos + 'px';
@@ -24,8 +25,8 @@ function catClicked() {
 function createNewCat() {
     var newCat = document.createElement("img");
     newCat.src = "cat.png"
-    newCat.width = 100;
-    newCat.height = 100;
+    newCat.width = 50;
+    newCat.height = 50;
     newCat.style.left = "100px";
     newCat.style.top = "100px";
     var container = document.getElementById("new-cat-image")
@@ -33,3 +34,7 @@ function createNewCat() {
     console.log("Created New Cat Image");
 }
 
+function removeNewCat(newCat) {
+    var newCat = document.getElementById("new-cat-image");
+    newCat.remove();
+}
