6d77fcc692e91adc490992e33ae20098e665b139
implaymo <goncalomoreirasilva@gmail.com>
2024-06-28 11:42:06 +0100

    Manage to get pos of cat. Need to make cats fly of the main image to a random direction.

--- CatGameWesbite/src/main/resources/static/javascript/script.js
+++ CatGameWesbite/src/main/resources/static/javascript/script.js
@@ -1,5 +1,20 @@
 
+var id = null;
+
 function catMoving() {
-    alert("CLCIK CAT");
-    console.log("JS WORKING)");
+    var cat = document.getElementById("cat");
+    var catPos = cat.getBoundingClientRect();
+    console.log("Position cat: " + catPos.x + catPos.y);
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (catPos == 350) {
+            clearInterval(id);
+        }
+        else {
+            catPos++;
+            cat.style.top = catPos + 'px';
+            cat.style.left = catPos + 'px';
+        }
+    }
 }
