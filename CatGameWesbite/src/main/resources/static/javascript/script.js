e7b38457ac2ad057e092fb4aa646ac08e78ee4e0
implaymo <goncalomoreirasilva@gmail.com>
2024-06-28 14:57:21 +0100

    Manage to create new images of a cat. Need fixing

--- CatGameWesbite/src/main/resources/static/javascript/script.js
+++ CatGameWesbite/src/main/resources/static/javascript/script.js
@@ -1,21 +1,35 @@
 
 var id = null;
 
-function catMoving() {
+function catClicked() {
     var cat = document.getElementById("cat-image");
     var catPos = 0;
     clearInterval(id);
     id = setInterval(frame, 10);
     function frame() {
         if (catPos >= 700) {
+            console.log("Cat reach the limits.");
             clearInterval(id);
         }
         else {
+            console.log("Cat is going to move.");
+            createNewCat();
             catPos++;
             cat.style.left = catPos + 'px';
             cat.style.top = catPos + 'px';
-            console.log("CAT LEFT " + cat.style.left)
         }
     }
+}
+
+function createNewCat() {
+    var newCat = document.createElement("img");
+    newCat.src = "cat.png"
+    newCat.width = 100;
+    newCat.height = 100;
+    newCat.style.left = "100px";
+    newCat.style.top = "100px";
+    var container = document.getElementById("new-cat-image")
+    container.appendChild(newCat);
+    console.log("Created New Cat Image");
 }
 
