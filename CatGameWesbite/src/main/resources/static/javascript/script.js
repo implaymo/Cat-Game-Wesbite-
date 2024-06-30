bfc1ec51e279b674993d8d04c2c1e5029af9fb16
implaymo <goncalomoreirasilva@gmail.com>
2024-06-30 12:18:38 +0100

    Implement count of times user clicks cat.

--- CatGameWesbite/src/main/resources/static/javascript/script.js
+++ CatGameWesbite/src/main/resources/static/javascript/script.js
@@ -1,17 +1,19 @@
 
-var id = null;
-var leftLimit = 0;
-var topLimit = 0;
-var rightLimit = window.innerWidth;
-var bottomLimit = window.innerHeight;
+let id = null;
+let leftLimit = 0;
+let topLimit = 0;
+let rightLimit = window.innerWidth;
+let bottomLimit = window.innerHeight;
+let timesClick = 0;
 
 function catClicked() {   
-    var randomNumber = Math.floor(Math.random() * 4);
-    var newCat = createNewCat()
-    var catPosX = window.innerWidth / 2;
-    var catPosY = window.innerHeight / 2;
+    timesClick++;
+    let randomNumber = Math.floor(Math.random() * 4);
+    let newCat = createNewCat()
+    let catPosX = window.innerWidth / 2;
+    let catPosY = window.innerHeight / 2;
     clearInterval(id);
-    id = setInterval(frame, 0.1);
+    id = setInterval(frame, 10);
     function frame() {
         if (catPosX >= rightLimit || catPosY >= bottomLimit || catPosX <= leftLimit || catPosY <= topLimit) {
             console.log("Cat reach the limits.");
@@ -20,44 +22,42 @@ function catClicked() {
             clearInterval(id);
         }
         else {
+            console.log("Cat is going to move.");
             if (randomNumber == 0) {
-                console.log("Cat is going to move.");
                 catPosX++;
                 catPosY++;
                 newCat.style.left = catPosX + 'px';
             }
             else if (randomNumber == 1) {
-                console.log("Cat is going to move.");
                 catPosX++;
                 catPosY++;
                 newCat.style.right = catPosX + 'px';
             }
             else if (randomNumber == 2) {
-                console.log("Cat is going to move.");
                 catPosX++;
                 catPosY++;
                 newCat.style.bottom = catPosY + 'px';
             }
             else if (randomNumber == 3) {
-                console.log("Cat is going to move.");
                 catPosX++;
                 catPosY++;
                 newCat.style.top = catPosY + 'px';
             }
         }
     }
+    console.log("TIMES CLICKED: " + timesClick)
 }
 
 catId = 0;
 
 function createNewCat() {
-    var newCat = document.createElement("img");
+    let newCat = document.createElement("img");
     newCat.src = "cat.png";
     newCat.id = "new-cat-image " + catId;
     newCat.style.position = "absolute";
     newCat.width = 50;
     newCat.height = 50;
-    var container = document.getElementById("new-cat-image")
+    let container = document.getElementById("new-cat-image")
     container.appendChild(newCat);
     console.log("Created New Cat Image");
     catId++;
@@ -65,11 +65,6 @@ function createNewCat() {
 }
 
 function removeNewCat(newCatId) {
-    var catToRemove = document.getElementById(newCatId);
+    let catToRemove = document.getElementById(newCatId);
     catToRemove.remove();
-}
-
-function moveCat() {
-    catPosX++;
-    catPosY++;
 }
