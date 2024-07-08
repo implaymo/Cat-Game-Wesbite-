e6b5f7e5caf20a0a84a29d361d6e3240e9460539
implaymo <goncalomoreirasilva@gmail.com>
2024-07-08 22:29:37 +0100

    Implement password restrictions when user registers

--- CatGameWesbite/src/main/java/com/catgame/CatGameWesbite/Main.java
+++ CatGameWesbite/src/main/java/com/catgame/CatGameWesbite/Main.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class Main {
-    @Autowired
-    private Database database;
     
     @RequestMapping("/")
     public String homepage(){
--- CatGameWesbite/src/main/java/com/catgame/CatGameWesbite/RegistrationController.java
+++ CatGameWesbite/src/main/java/com/catgame/CatGameWesbite/RegistrationController.java
@@ -19,12 +19,18 @@ public class RegistrationController {
         String email = request.getParameter("email");
         String password1 = request.getParameter("password1");
         String password2 = request.getParameter("password2");
-        if (password1.equals(password2)) {
+        if (password1.equals(password2) && password1.length() >= 8) {
             String hashPassword = BCrypt.hashpw(password1, BCrypt.gensalt());
             System.out.println("HASH " + hashPassword);
             database.insertUser(name, email, hashPassword, 0);
         }
+        else 
+        {
+            System.out.println("Easy password. Try again.");
+        }
         return "redirect:/";  
     }
+
+
 }
 
