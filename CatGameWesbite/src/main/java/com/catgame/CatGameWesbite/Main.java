0d14d3557ea02fb3bda5f79e3c56a6a26e1d1dba
implaymo <goncalomoreirasilva@gmail.com>
2024-07-08 22:30:43 +0100

    Implement password restrictions when user registers

--- CatGameWesbite/src/main/java/com/catgame/CatGameWesbite/Main.java
+++ CatGameWesbite/src/main/java/com/catgame/CatGameWesbite/Main.java
@@ -1,6 +1,5 @@
 package com.catgame.CatGameWesbite;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 
