3fd8c6c052fd053e5d09ac3f5d20040261916514
implaymo <goncalomoreirasilva@gmail.com>
2024-07-08 23:05:25 +0100

    Implement password requirements when registering. Need improvement.

--- CatGameWesbite/src/main/java/com/catgame/CatGameWesbite/Main.java
+++ CatGameWesbite/src/main/java/com/catgame/CatGameWesbite/Main.java
@@ -1,10 +1,14 @@
 package com.catgame.CatGameWesbite;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 
 @Controller
 public class Main {
+    
+    @Autowired
+    private Database database;
     
     @RequestMapping("/")
     public String homepage(){
