8b1b23f89bcfd9364c38b99f30dd54f0f1d2173a
implaymo <goncalomoreirasilva@gmail.com>
2024-07-17 17:22:52 +0100

    Setup some requirements for authentication.

--- CatGameWesbite/src/main/java/com/catgame/CatGameWesbite/SecurityConfiguration.java
+++ CatGameWesbite/src/main/java/com/catgame/CatGameWesbite/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfiguration {
@@ -18,7 +17,14 @@ public class SecurityConfiguration {
                 .anyRequest().authenticated()
                 
             )
-            .httpBasic(withDefaults());
+            .formLogin(form -> form
+                .loginPage("/login") 
+                .defaultSuccessUrl("/", true) 
+                .permitAll()
+            )
+            .logout(logout -> logout
+                .permitAll()
+            );
         return http.build();
     }
 
