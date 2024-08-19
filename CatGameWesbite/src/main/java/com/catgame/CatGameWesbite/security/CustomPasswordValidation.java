b7469c9f07ca7149fdcfb8fe41093dc2550d908c
implaymo <goncalomoreirasilva@gmail.com>
2024-08-19 14:48:48 +0100

    Set special_chars as a static final variable.

--- CatGameWesbite/src/main/java/com/catgame/CatGameWesbite/security/CustomPasswordValidation.java
+++ CatGameWesbite/src/main/java/com/catgame/CatGameWesbite/security/CustomPasswordValidation.java
@@ -5,7 +5,10 @@ import org.apache.logging.log4j.Logger;
 
 public class CustomPasswordValidation {
 
-    private static final Logger logger = LogManager.getLogger(CustomPasswordValidation.class);    
+    private static final Logger logger = LogManager.getLogger(CustomPasswordValidation.class); 
+    
+    private static final String special_chars = "!(){}[]:;<>?,@#$%^&*+=_-~`|./'";
+
 
 
     public boolean passwordStrenght(String password) {
@@ -14,7 +17,6 @@ public class CustomPasswordValidation {
         boolean containsDigit = false;
         boolean containsSpecialChar = false; 
         boolean minLength = false;
-        String special_chars = "!(){}[]:;<>?,@#$%^&*+=_-~`|./'";
         char[] ch = password.toCharArray();
 
         if (password.length() >= 8) {
@@ -37,7 +39,7 @@ public class CustomPasswordValidation {
             }
         }
 
-        if (containsDigit && containsUpperChar && containsSpecialChar && containsLowerChar) {
+        if (minLength && containsDigit && containsUpperChar && containsSpecialChar && containsLowerChar) {
             logger.info("Password checks all the requirements.");
             return true;
         } 
